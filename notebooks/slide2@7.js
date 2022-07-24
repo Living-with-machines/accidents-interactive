6b82d0c19dd76cf5024cc3c99bee77d83e6590cf
Mia <mia.ridge@bl.uk>
2022-07-24 16:01:25 +0100

    Tweaked intro text

--- notebooks/slide2@7.js
+++ notebooks/slide2@7.js
@@ -1,7 +1,7 @@
 function _1(htl) {
   return (
     htl.html`<div class="slide">
-  <p>Where did newspapers report accidents involving machinery? We asked online volunteers to help us find out. Explore sample data.</p>
+  <p>Where did newspapers report accidents involving machinery? We asked online volunteers to tag articles from selected regional newspapers. Explore our sample data.</p>
   <div class="d-flex flex-row align-items-baseline">
     <p><a class="me-4 btn btn-lg btn-warning rounded-4 shadow" href="slide3.html"><b>When</b> and <b>where</b> did newspapers report accidents?</a></p>
     <p><a class="me-4 btn btn-lg btn-warning rounded-4 shadow" href="slide4.html"><b>Who</b> was affected?</a></p>
