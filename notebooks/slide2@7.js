e86e5e7833c2eeacb20596cdcf703753b96b452d
Kalle Westerling <kalle.westerling@bl.uk>
2022-07-25 09:51:40 +0100

    Updating text on slide 2 to reflect changes

--- notebooks/slide2@7.js
+++ notebooks/slide2@7.js
@@ -3,7 +3,7 @@ function _1(htl) {
     htl.html`<div class="slide">
   <p>Where did newspapers report accidents involving machinery? We asked online volunteers to tag articles from selected regional newspapers. Explore our early results.</p>
   <div class="d-flex flex-row align-items-baseline">
-    <p><a class="me-4 btn btn-lg btn-warning rounded-4 shadow" href="slide3.html"><b>When</b> and <b>where</b> did newspapers report accidents?</a></p>
+    <p><a class="me-4 btn btn-lg btn-warning rounded-4 shadow" href="slide3.html"><b>When</b> did newspapers report accidents?</a></p>
     <p><a class="me-4 btn btn-lg btn-warning rounded-4 shadow" href="slide4.html"><b>Who</b> was affected?</a></p>
     <p><a class="btn btn-lg btn-warning rounded-4 shadow" href="slide5.html">Factories or homes? Explore <b>accident sites</b></a></p>
   </div>
